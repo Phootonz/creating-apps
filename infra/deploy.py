dc93a22734f5f314da49be263a8323728de75649
Jeff Snyder <tc.photon@gmail.com>
2025-08-31 15:13:32 -0400

    added params to dict

--- infra/deploy.py
+++ infra/deploy.py
@@ -3,5 +3,5 @@ import sys
 if __name__ == "__main__":
     issue_body = sys.argv[1]
     tokens = issue_body.split('\n')
-    for token in tokens:
-        print(token)
+    app_params = {item[0]:item[1] for item.split(':') in tokens}
+    print(app_params)
