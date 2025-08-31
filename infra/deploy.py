51a3dbcb2952ff77c3fa92663e73e18fbc9f0639
Jeff Snyder <tc.photon@gmail.com>
2025-08-31 15:14:59 -0400

    fixed split

--- infra/deploy.py
+++ infra/deploy.py
@@ -3,5 +3,5 @@ import sys
 if __name__ == "__main__":
     issue_body = sys.argv[1]
     tokens = issue_body.split('\n')
-    app_params = {item[0]:item[1] for item.split(':') in tokens}
+    app_params = {item.split(':')[0]:item.split(':')[1] for item in tokens}
     print(app_params)
