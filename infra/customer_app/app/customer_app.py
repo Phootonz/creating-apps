ec07a116771be9bc730b1d115fb2607954ba1cf7
Jeff Snyder <tc.photon@gmail.com>
2025-08-31 23:09:43 -0400

    added chart for customer page. added custoemr page server

---
+++ infra/customer_app/app/customer_app.py
@@ -0,0 +1,32 @@
+import os
+import json
+import yaml
+from typing import Annotated
+from fastapi import FastAPI, Request, Depends, Form, HTTPException, status
+from fastapi.responses import StreamingResponse, HTMLResponse, FileResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from asyncio import sleep
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get(f"/customer/{os.getenv('CUSTOMER_NAME')}")
+async def create_form(request: Request):
+    return templates.TemplateResponse("landing.html", 
+                                      {"request": request,
+                                       "name": os.getenv("CUSTOMER_NAME"),
+                                       "motto": os.getenv("CUSTOMER_MOTTO")})
